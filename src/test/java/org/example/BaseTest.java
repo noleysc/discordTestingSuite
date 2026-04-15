e324faa285e15966612c610c6da85c1ff8ff0b79
Chris Cartaya <98562885+chris-cartaya@users.noreply.github.com>
2026-04-15 16:00:22 -0400

    build: add shared BaseTest and ignore IDE files

--- src/test/java/org/example/BaseTest.java
+++ src/test/java/org/example/BaseTest.java
@@ -1,4 +1,50 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+/**
+ * BaseTest
+ *
+ * This class provides shared setup and teardown functionality
+ * for all Selenium test classes in the project.
+ *
+ * All test classes should extend this class to inherit:
+ * - WebDriver instance
+ * - WebDriverWait instance
+ * - Actions instance
+ */
 public class BaseTest {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected Actions actions;
+
+    @BeforeMethod
+    public void setUp() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("--disable-blink-features=AutomationControlled");
+
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        actions = new Actions(driver);
+
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
