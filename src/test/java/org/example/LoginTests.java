83505af5a39bd993311183ea446c67379a9abe1a
Chris Cartaya <98562885+chris-cartaya@users.noreply.github.com>
2026-04-15 16:23:21 -0400

    refactor: move LoginTests to shared BaseTest structure

--- src/test/java/org/example/LoginTests.java
+++ src/test/java/org/example/LoginTests.java
@@ -1,61 +1,49 @@
 package org.example; // This must be the very first line [cite: 83]
 
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.*;
-import org.testng.annotations.*;
-import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
 
-public class LoginTests { // All your code MUST be inside these main brackets
-    WebDriver driver;
-    WebDriverWait wait;
-
-    @BeforeMethod
-    public void setup() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--incognito");
-        options.addArguments("--disable-blink-features=AutomationControlled");
-
-        driver = new ChromeDriver(options); // Uses Selenium WebDriver [cite: 72]
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Addresses dynamic elements [cite: 10, 49]
-        driver.get("https://discord.com/login");
-    }
+/**
+ * LoginTests covers core authentication-related UI flows on Discord's login page.
+ */
+public class LoginTests extends BaseTest {
 
     @Test
     public void testValidLogin() {
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("email"))).sendKeys("nstilwell863@gmail.com");
-        driver.findElement(By.name("password")).sendKeys("testPassword!@#");
+        driver.get("https://discord.com/login");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("email")))
+                .sendKeys("discord.testing.fgcu@gmail.com");
+        driver.findElement(By.name("password")).sendKeys("DiscordTest123!");
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']"))).click();
     }
 
     @Test
     public void testInvalidPassword() {
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("email"))).sendKeys("nstilwell863@gmail.com");
+        driver.get("https://discord.com/login");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("email")))
+                .sendKeys("discord.testing.fgcu@gmail.com");
         driver.findElement(By.name("password")).sendKeys("wrongPass");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
     }
 
     @Test
     public void testEmptyFields() {
+        driver.get("https://discord.com/login");
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']"))).click();
     }
 
     @Test
     public void testRegisterNavigation() {
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(text(), 'Register')]"))).click();
+        driver.get("https://discord.com/login");
+        wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[contains(text(), 'Register')]"))).click();
     }
 
     @Test
     public void testForgotPasswordLink() {
-        // Broad search to handle automation constraints [cite: 10]
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(translate(text(), 'FORGOT', 'forgot'), 'forgot')]"))).click();
+        driver.get("https://discord.com/login");
+        wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[contains(translate(text(), 'FORGOT', 'forgot'), 'forgot')]"))).click();
     }
-
-    @AfterMethod
-    public void teardown() {
-        if (driver != null) {
-            driver.quit(); // Cleanly closes execution [cite: 30, 64]
-        }
-    }
-} // Final bracket closes the class
+}
